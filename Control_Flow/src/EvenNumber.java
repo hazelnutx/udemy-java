80d564b9455562ebc9a6e302577201a86cb7db31
hazelnutx <raulrusu46@gmail.com>
2019-09-17 15:15:28 +0300

    made it to count how many even numbers are and stop at 5

--- Control_Flow/src/EvenNumber.java
+++ Control_Flow/src/EvenNumber.java
@@ -5,14 +5,20 @@ public class EvenNumber {
 		
 		int number = 4;
 		int finishNumber = 20;
+		int count = 0;
 		
 		while(number <= finishNumber) {
 			number++;
 			if(!isEvenNumber(number)) { //So it won't print out the false values
+				if(count == 5) {
+					break;
+				}
+				count++;
 				continue; // won't let it go pass this if there is not an even number e.g : 7 and it will just go back to top and continue with the new number.
 			}
 			System.out.println("The even number is " + number);
 		}
+		System.out.println("Total numbers = " + count);
 	}
 	
 	

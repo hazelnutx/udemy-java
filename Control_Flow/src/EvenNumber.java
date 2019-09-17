e44d62bfa06cb85f6845059e348b0095d39b38cf
hazelnutx <raulrusu46@gmail.com>
2019-09-17 15:01:15 +0300

    Learned about how to pass

--- Control_Flow/src/EvenNumber.java
+++ Control_Flow/src/EvenNumber.java
@@ -2,7 +2,17 @@
 public class EvenNumber {
 	
 	public static void main(String[] args) {
-		System.out.println(isEvenNumber(3));
+		
+		int number = 4;
+		int finishNumber = 20;
+		
+		while(number <= finishNumber) {
+			number++;
+			if(!isEvenNumber(number)) { //So it won't print out the false values
+				continue; // won't let it go pass this if there is not an even number e.g : 7 and it will just go back to top and continue with the new number.
+			}
+			System.out.println("The even number is " + number);
+		}
 	}
 	
 	

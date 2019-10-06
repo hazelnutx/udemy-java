bd7e2bbf35c268d1b58f225ccfeabc0a83f591c8
hazelnutx <raulrusu46@gmail.com>
2019-10-06 23:36:44 +0300

    Polymorphism challenge

--- Polymorphism/polymorphism/Car.java
+++ Polymorphism/polymorphism/Car.java
@@ -1,5 +1,84 @@
 package polymorphism;
 
-public class Car {
+class Car {
+	private boolean engine;
+	private int cylinders;
+	private int wheels;
+	private String name;
 
+	
+	public Car(int cylinders, String name) {
+		this.engine = true;
+		this.cylinders = cylinders;
+		this.wheels = 4;
+		this.name = name;
+	}
+	
+	public String startEngine() {		
+		return "Car -> Engine starting";
+	}
+	
+	public String accelerate() {
+		return "Car -> Accelerate";
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+
+	public String getName() {
+		return name;
+	}
+		
 }
+
+class Nissan extends Car {
+	
+	public Nissan() {
+		super(8, "Nissan 350z");
+	}
+	
+	@Override
+	public String startEngine() {
+		return "350z V8 Engine Starting!";
+	}
+
+	@Override
+	public String accelerate() {
+		return "Nissan -> Accelerate";
+	}
+	
+}
+
+class Zafira extends Car {
+	public Zafira() {
+		super(4, "Opel Zafira");
+	}
+	
+	@Override 
+	public String startEngine() {
+		return "Zafira -> Start Engine";
+	}
+	
+	@Override
+	public String accelerate() {
+		return "Opel -> Accelerates";
+	}
+}
+
+class Bmw extends Car {
+	public Bmw() {
+		super(6, "BMW M6");
+	}
+	
+	@Override
+	public String startEngine() {
+		return "BMW -> Start Engine";
+	}
+	
+	@Override
+	public String accelerate() {
+		return "BMW -> Accelerates";
+	}
+}
+
---
+++ Polymorphism/polymorphism/Main.java
@@ -0,0 +1,30 @@
+package polymorphism;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		for(int i = 1; i < 6; i++) {
+			Car car = randomCar();
+			System.out.println("Car # " + i + ":" + car.getName() + "\n" + "Engine: " + car.startEngine() + "\n");
+		}
+	}
+	
+	
+	public static Car randomCar() {
+		int randomNumber = (int) (Math.random() * 3) + 1;
+		System.out.println("Random number " + randomNumber);
+		
+		switch(randomNumber) {
+			case 1:
+				return new Nissan();
+				
+			case 2:
+				return new Zafira();
+			
+			case 3:
+				return new Bmw();
+
+		}
+		return null;
+	}
+}

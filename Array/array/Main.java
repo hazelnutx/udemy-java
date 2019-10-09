db19729e5fc1a5fe6a575b09c0619af312f13fcf
hazelnutx <raulrusu46@gmail.com>
2019-10-09 15:41:06 +0300

    Using Arrays and Collections library to sort in a decending order an array

--- Array/array/Main.java
+++ Array/array/Main.java
@@ -1,21 +1,22 @@
 package array;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 	
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int[] myArray = getIntegers(3);
+		Integer[] myArray = getIntegers(3);
 		
 		printArray(myArray);
 		sortIntegers(myArray);
 	}
 	
-	public static int[] getIntegers(int numbers) {
-		System.out.println("Enter numbers: \r");
-		int[] values = new int[numbers];
+	public static Integer[] getIntegers(int numbers) {
+		System.out.println("Enter numbers: ");
+		Integer[] values = new Integer[numbers];
 		
 		for(int i = 0; i < values.length; i++) {
 			values[i] = scanner.nextInt();
@@ -23,13 +24,19 @@ public class Main {
 		return values;
 	}
 	
-	public static void printArray(int[] element) {
+	public static void printArray(Integer[] element) {
 		for(int x = 0; x < element.length; x++) {
 			System.out.println("Element " + x + " has number " + element[x]);
 		}		
 	}
 	
-	public static void sortIntegers(int[] toSort) {
-		System.out.println(Arrays.sort(toSort));
+	public static void sortIntegers(Integer[] inputArray) {
+		
+		Arrays.sort(inputArray, Collections.reverseOrder());
+		String input = Arrays.toString(inputArray);
+		for(int i = 0; i < inputArray.length; i++) {
+			System.out.println("Element: " + i + " new number is " + input);
+		}
+		scanner.close();
 	}
 }

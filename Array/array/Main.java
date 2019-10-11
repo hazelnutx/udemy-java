2a355104deefb1af3f6ff27a826d59491a70388b
hazelnutx <raulrusu46@gmail.com>
2019-10-11 16:32:30 +0300

    revised the sort array method - really nice

--- Array/array/Main.java
+++ Array/array/Main.java
@@ -8,15 +8,14 @@ public class Main {
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Integer[] myArray = getIntegers(3);
-		
-		printArray(myArray);
-		sortIntegers(myArray);
+		int[] myArray = getIntegers(3);
+		int[] sorted = sortIntegers(myArray);
+		printArray(sorted); // you can change from myArray to sorted
 	}
 	
-	public static Integer[] getIntegers(int numbers) {
+	public static int[] getIntegers(int numbers) {
 		System.out.println("Enter numbers: ");
-		Integer[] values = new Integer[numbers];
+		int[] values = new int[numbers];
 		
 		for(int i = 0; i < values.length; i++) {
 			values[i] = scanner.nextInt();
@@ -24,19 +23,34 @@ public class Main {
 		return values;
 	}
 	
-	public static void printArray(Integer[] element) {
+	public static void printArray(int[] element) {
 		for(int x = 0; x < element.length; x++) {
 			System.out.println("Element " + x + " has number " + element[x]);
 		}		
 	}
 	
-	public static void sortIntegers(Integer[] inputArray) {
+	public static int[] sortIntegers(int[] array) {
+		int[] sortedArray = new int[array.length];
 		
-		Arrays.sort(inputArray, Collections.reverseOrder());
-		String input = Arrays.toString(inputArray);
-		for(int i = 0; i < inputArray.length; i++) {
-			System.out.println("Element: " + i + " new number is " + input);
+		for(int i = 0; i < array.length; i++) {
+			sortedArray[i] = array[i];
+		}
+		
+		boolean flag = true; // put this when you want to check if something is true go on and if not, put it to false so it stops
+		int temp;
+		
+		while(flag) {
+			flag = false;
+			for(int i = 0; i < sortedArray.length - 1; i++) {
+				if(sortedArray[i] < sortedArray[i+1]) {
+					temp = sortedArray[i];
+					sortedArray[i] = sortedArray[i+1];
+					sortedArray[i+1] = temp;
+					flag = true;
+				}
+			}
 		}
 		scanner.close();
+		return sortedArray;		
 	}
 }

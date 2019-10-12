71c88c011b1c5860020b903a0788cd6e8597b9fd
hazelnutx <raulrusu46@gmail.com>
2019-10-12 21:52:27 +0300

    reverse array comments to better understand the process

--- Array/array/ReverseArray.java
+++ Array/array/ReverseArray.java
@@ -13,6 +13,14 @@ public class ReverseArray {
 	
 	public static void reverse(int[] array) {
 		int maxIndex = array.length - 1;
+/*
+ * Always put -1 if you want to check the last number or the last space in an array because it start from 0
+ * so 0 1 2 3 4 you have a length of 5, but if you want to access the last element you need to go something
+ * like this 5 - 1 = 4 === last element.
+ * 
+ * The halfLength is divided by 2, because you want to get to the middle of the array, and kinda flip on it.
+ * 
+ */
 		int halfLength= array.length / 2;
 		
 		for(int i = 0; i < halfLength; i++) {
